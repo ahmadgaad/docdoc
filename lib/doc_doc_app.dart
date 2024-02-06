dd882b3919dfb1c277d41f4c19944641758ff346
Ahmed Gad <128515678+ahmadgaad@users.noreply.github.com>
2024-02-06 20:13:07 +0200

    Initial commit

---
+++ lib/doc_doc_app.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class DocDocApp extends StatelessWidget {
+  const DocDocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
